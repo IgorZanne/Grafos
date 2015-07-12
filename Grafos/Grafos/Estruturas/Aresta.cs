5f2776c390a25b558245d8528d092787f34c303c
Igor Zanne <igor.zanne@gmail.com>
2015-07-12 19:52:30 -0300

    Grafos

---
+++ Grafos/Grafos/Estruturas/Aresta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos.Estruturas
+{
+    public class Aresta
+    {
+        public int Peso { get; set; }
+        public Vertice Origem { get; set; }
+        public Vertice Destino { get; set; }
+    }
+}
---
+++ Grafos/Grafos/Estruturas/Grafo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos.Estruturas
+{
+    public class Grafo
+    {
+        public bool Direcionado { get; set; }
+        public List<Vertice> Vertices { get; set; }
+        public List<Aresta> Arestas { get; set; }
+
+        public Grafo(bool direcionado)
+        {
+            this.Direcionado = direcionado;
+            this.Vertices = new List<Vertice>();
+            this.Arestas = new List<Aresta>();
+        }
+
+        public Grafo Ler(string caminhoArquivo, bool direcionado)
+        {
+            var arquivo = File.ReadAllLines(caminhoArquivo);
+            var retorno = new Grafo(direcionado);
+
+            var primeiraParte = true;
+            foreach (var linha in arquivo)
+            {
+                if (linha.Equals("#"))
+                    primeiraParte = false;
+                if (primeiraParte)
+                    retorno.Vertices.Add(new Vertice());
+                else
+                    retorno.Arestas.Add(new Aresta());
+            }
+
+            return retorno;
+        }
+    }
+}
