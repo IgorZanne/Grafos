1eab42784ad64e40fcf1cffbb4c0347a1904fc5b
Bruno Henrique Kunieda <brunoohk@gmail.com>
2015-07-25 20:58:16 -0300

    implementação do dfs

--- Grafos/Grafos/Algoritmos/Scc.cs
+++ Grafos/Grafos/Algoritmos/Scc.cs
@@ -9,8 +9,53 @@ namespace Grafos.Algoritmos
 {
     public class Scc : Algoritmo
     {
+        private void dfs(Grafo grafo)
+        {
+            foreach(var vertice in grafo.Vertices)
+            {
+                vertice.Cor = CoresEnum.Branco;
+                vertice.Pai = null;
+            }
+            int tempo = 0;
+            foreach(var vertice in grafo.Vertices)
+            {
+                if (vertice.Cor == CoresEnum.Branco)
+                {
+                    dfsVisit(vertice, tempo);
+                }
+            }
+        }
+
+        private void dfsVisit(Vertice U, int tempo)
+        {
+            tempo = tempo++;
+            U.Cor = CoresEnum.Cinza;
+            U.Descoberta = tempo;
+            foreach(Vertice vertice in U.Adj)
+            {
+                if (vertice.Cor == CoresEnum.Branco)
+                {
+                    vertice.Pai = U;
+                    dfsVisit(vertice, tempo);
+                }
+            }
+            U.Cor = CoresEnum.Preto;
+            tempo = tempo++;
+            //U.finalizacao = tempo;
+        }
+
+        private void calculaTransposto(Grafo grafo)
+        {
+            Grafo grafoTransposto = new Grafo();
+            foreach(var vertice in grafo.Vertices)
+            {
+                
+            }
+        }
         public override IEnumerable<string> Executar(Grafo grafo)
         {
+            dfs(grafo);
+
             throw new NotImplementedException();
         }
     }
