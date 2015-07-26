b9181b0138dec7ee3d6c006f20793745c38c4234
Igor Zanne <igor.zanne@gmail.com>
2015-07-25 23:14:43 -0300

    - Implementação do SCC

--- Grafos/Grafos/Algoritmos/Scc.cs
+++ Grafos/Grafos/Algoritmos/Scc.cs
@@ -11,27 +11,35 @@ namespace Grafos.Algoritmos
     public class Scc : Algoritmo
     {
         private Grafo grafoCorrente;
+        private int tempo;
+        private string componente;
+        private List<string> retorno = new List<string>();
 
         private void dfs(Grafo grafo, bool decrescente)
         {
+            grafo.Direcionado = true;
             foreach(var vertice in grafo.Vertices)
             {
                 vertice.Cor = CoresEnum.Branco;
                 vertice.Pai = null;
             }
-            int tempo = 0;
-            foreach (var vertice in (decrescente ? grafo.Vertices : grafo.Vertices.OrderByDescending(e => e.Finalizacao).ToList()))
+            tempo = 0;
+            foreach (var vertice in (!decrescente ? grafo.Vertices : grafo.Vertices.OrderByDescending(e => e.Finalizacao).ToList()))
             {
                 if (vertice.Cor == CoresEnum.Branco)
                 {
-                    dfsVisit(vertice, tempo);
+                    componente = string.Empty;
+                    dfsVisit(vertice);
+                    if (decrescente)
+                        retorno.Add(componente);
                 }
             }
         }
 
-        private void dfsVisit(Vertice u, int tempo)
+        private void dfsVisit(Vertice u)
         {
-            tempo = tempo++;
+            componente = componente + " " + u.Id;
+            tempo++;
             u.Cor = CoresEnum.Cinza;
             u.Descoberta = tempo;
             foreach(Vertice vertice in grafoCorrente.GetAdj(u.Id))
@@ -39,11 +47,11 @@ namespace Grafos.Algoritmos
                 if (vertice.Cor == CoresEnum.Branco)
                 {
                     vertice.Pai = u;
-                    dfsVisit(vertice, tempo);
+                    dfsVisit(vertice);
                 }
             }
             u.Cor = CoresEnum.Preto;
-            tempo = tempo++;
+            tempo++;
             u.Finalizacao = tempo;
         }
 
@@ -51,11 +59,10 @@ namespace Grafos.Algoritmos
         {
             grafoCorrente = grafo;
             dfs(grafo, false);
-            var tranposto = GrafoHelper.GetTransposto(grafo);
-            dfs(tranposto, true);
+            var transposto = GrafoHelper.GetTransposto(grafo);
+            dfs(transposto, true);
 
-
-            throw new NotImplementedException();
+            return retorno;
         }
     }
 }
