bdf2c624dab3d6ad31d9134fa008ec941bb28ac5
Bruno Henrique Kunieda <brunoohk@gmail.com>
2015-07-26 23:48:56 -0300

    implementação do bf

--- Grafos/Grafos/Algoritmos/Bf.cs
+++ Grafos/Grafos/Algoritmos/Bf.cs
@@ -9,9 +9,45 @@ namespace Grafos.Algoritmos
 {
     public class Bf : Algoritmo
     {
+        private void initialize(Grafo grafo)
+        {
+            foreach(var vertice in grafo.Vertices)
+            {
+                vertice.Descoberta = Int32.MaxValue;
+                vertice.Pai = null;
+            }
+        }
+
+        private void relax(Vertice u, Vertice v, int w)
+        {
+            if (v.Descoberta > (u.Descoberta+w))
+            {
+                v.Descoberta = u.Descoberta + w;
+                v.Pai = u;
+            }
+        }
         public override IEnumerable<string> Executar(Grafo grafo)
         {
-            throw new NotImplementedException();
+            initialize(grafo);
+            Vertice s = grafo.Vertices.First();
+            int i;
+            for (i = 1; i < grafo.Vertices.Count -1; i++ )
+            {
+                foreach(var aresta in grafo.Arestas)
+                {
+                    relax(aresta.Origem, aresta.Destino, aresta.Peso);
+                }
+            }
+
+            foreach(var aresta in grafo.Arestas)
+            {
+                if (aresta.Destino.Descoberta > aresta.Origem.Descoberta + aresta.Peso)
+                {
+                    //return = false;
+                }
+            }
+            //return = true;
+                throw new NotImplementedException();
         }
     }
 }
